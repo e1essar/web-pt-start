5284b4977db830c97e22fcd6feb3f4390bf1ba7b
e1essar <igorvaliulin770@gmail.com>
2024-04-24 23:49:10 +0700

    add: php

---
+++ profile.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Арагорн II Элессар</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://fonts.cdnfonts.com/css/aniron" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="container nav_bar">
+        <div class="row">
+            <div class="col-3 nav_logo"></div>
+            <div class="col-9">
+                <div class="nav_text">Информация</div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container content">
+        <div class="row">
+            <div class="col-12">
+                <h1>Арагорн II Элессар</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-8">
+                <p>А́рагорн (синд. Aragorn), коронованный как Эле́ссар (кв. Elessar) — человек-дунадан, 
+                    сын Араторна II и Гильраэн, шестнадцатый вождь Дунэдайн Севера, прямой потомок Исильдура, 
+                    старшего сына Элендиля и последнего Верховного короля Дунэдайн, 
+                    и единственный законный наследник трона Гондора.</p>
+                <p>
+                    Третий и последний в истории Средиземья человек, вступивший в брак с эльфийской девой 
+                    — его женой и королевой стала Арвен Ундомиэль, дочь Эльронда Полуэльфа.
+                </p>
+                <p>
+                    Арагорн стал величайшим из людей своей эпохи — он возглавлял Людей Запада в войне 
+                    против Тёмного Властелина Саурона и помог уничтожить Единое Кольцо Власти 
+                    (был одним из девяти членов Братства Кольца), а после победы воссоединил 
+                    королевства Арнор и Гондор в единое государство.
+                </p>
+                <div class="container">
+                    <div class="row">
+                        <div class="button_js col-12">
+                            <button id="myButton">Click me</button>
+                            <p id="demo"></p>
+                        </div> 
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="photo_info">
+                    <div class="my_photo"></div>
+                    <div class="info">
+                        <p><strong>Титулы:</strong> Вождь Дунэдайн Севера
+                            Верховный король Гондора и Арнора</p>
+                        <p><strong>Прозвища:</strong> Дунадан, Странник, Элессар, Эстель</p>
+                        <p><strong>Время правления:</strong> 2933 — 3019 Т. Э.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script type="text/javascript" src="js/button.js"></script>
+</body>
+</html>
